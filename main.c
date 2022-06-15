d8e4c079806b8bb6b3a43cf27038145a5d8c82e3
CaokimHanh <106530492+CaokimHanh@users.noreply.github.com>
2022-06-15 10:21:48 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num;
+	printf("Cac so chia het cho 5 va nho hon 100: \n\n");
+	for(num=100; num>0; num-=5)
+	printf("%d\n", num);
+	
+		return 0;
+}
